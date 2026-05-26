65f4f66c6d6dd06cbf333850bde1a3a51334271d
kenny1741 <kennedymwatha25@email.com>
2026-05-26 15:55:34 +0300

    fix cookiesToSet type error

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -7,13 +7,26 @@ export async function createServerSupabaseClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
+
+
+cookies: {
+  getAll() { return cookieStore.getAll(); },
+  setAll(cookiesToSet: { name: string; value: string; options?: object }[]) {
+    try {
+      cookiesToSet.forEach(({ name, value, options }) =>
+        cookieStore.set(name, value, options)
+      );
+
+
+      /*cookies: {
         getAll() { return cookieStore.getAll(); },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
+*/
+
           } catch {}
         },
       },
